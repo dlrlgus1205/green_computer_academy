a34293249cb2f3a1d80f0675fa8e13eebdf2e991
dlrlgus1205 <dlrlgus1205@gmail.com>
2022-08-30 16:49:24 +0900

    .

---
+++ behr/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  let winW = $(window).width()
+  let docW = $(document).width()
+  console.log(winW)
+  console.log(docW)
+
+  // $('header').addClass('fixed')
+  // $('header').removeClass('fixed')
+
+  // TOP 버튼을 클릭했을 때 상단으로 스크롤
+  $('aside button').on('click', function(){
+    console.log('눌렀어')
+    // $(window).scrollTop(100)
+    $('html,body').animate({
+      scrollTop : 0
+    },500)
+  })
+
+  // 스크롤 되면 header에 클래스 추가
+  let scrolling = $(window).scrollTop()
+
+  $(window).scroll(function(){
+    console.log(scrolling)
+    headerixed()
+  })
+
+  function headerixed(){
+    scrolling = $(window).scrollTop()
+    if(scrolling > 0){
+    $('header').addClass('fixed')
+  }
+  else {
+    $('header').removeClass('fixed')
+  }}
+})
