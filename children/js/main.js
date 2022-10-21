40558f29aaae72ac5abb76b209b9e15ba20ce920
dlrlgus1205 <dlrlgus1205@gmail.com>
2022-10-21 18:00:13 +0900

    .

--- children/js/main.js
+++ children/js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
 		navigationPosition: 'right', /* 위치 */
 		navigationTooltips: ['메인', '보건통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
 		showActiveTooltip: false, /* 현재 활성화된 페이지의 툴팁 보이기 */
+		
+		// scrollOverflow: false, 
 
 		afterLoad: function(origin, destination, direction, trigger){
 			//console.log(destination.index);
@@ -28,7 +30,33 @@ $(document).ready(function(){
 	/* 보건통계 숫자 카운팅 효과 */
 	function funCount(){
 		console.log('카운팅동작');
-		$('.count .rate li .num strong').counterUp();
+		$('.count .rate li .num strong').counterUp({
+			time: 1000    /* 전체 애니메이션 시간 */
+		});
 	}
+
+	/* 최근소식에 swiper효과 */
+	const swiper = new Swiper('.news .list', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			450: {    /* 640px 이상일때 적용 */
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			800: {    /* 640px 이상일때 적용 */
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			1200: {    /* 1200px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 40,
+			},
+		},
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.btn_paging', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		},
+	});
 
 });//document.ready
